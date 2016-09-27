ddf10a8ec7e9385c8e4aabb21a9f22fc4ee16500
cornellio <peter.cornell@me.com>
2016-09-27 16:43:02 -0700

    add argument parsing

--- oceanic.py
+++ oceanic.py
@@ -41,12 +41,37 @@ def parse_args():
     parser.add_argument('-a', help='Dashing authentication token',
                         required=True, dest='authtoken')
 
-    args = parser.parse_args()
-    return args
+    parser.add_argument('--host', help='Dashing host to send data to',
+                        required=True, dest='dashing_host')
+
+    parser.add_argument('-w', help='Widget to send data to',
+                        required=True, dest='widget')
+
+    parser.add_argument('-n', help='Number of data points to\
+                        plot on x-axis of graph',
+                        required=False, type=int, dest='num_recs', default=12)
+
+    parser.add_argument('-i', help='Interval. This is the number of values\
+                        to skip when plotting data points',
+                        required=False, type=int,
+                        dest='interval', default=1)
+
+    parser.add_argument('-x', '--skip_lookup', help='Do not lookup\
+                        current value, just graph historical\
+                        values', dest='skip_lookup', required=False,
+                        action='store_true')
+
+    parser.add_argument('-f', help='File to store values in. Defaults to the\
+                        name of the script, excluding the .py extension',
+                        required=False, dest='historyfile',
+                        default=sys.argv[0].strip('py') + "history")
+
+    return parser.parse_args()
 
 
-def main(arg):
-    pass
+def main():
+    args = parse_args()
+    print args
 
 if __name__ == '__main__':
     main()
